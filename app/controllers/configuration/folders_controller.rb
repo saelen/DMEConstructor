0733c4064111f96929dc3d3eee4300793b2f6f8b
saelen <mterhaar@gmail.com>
2011-10-01 12:26:45 -0500

    Continued menu work, initial link to configuration -> Folders

---
+++ app/controllers/configuration/folders_controller.rb
@@ -0,0 +1,5 @@
+class Configuration::FoldersController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 DMConstructor::Application.routes.draw do
+  
   get "folder/index"
 
   resources :projects
-
+  namespace :configuration do
+    resources :folders
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -59,4 +62,6 @@ DMConstructor::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
+  root :to => 'pages#index'
 end
